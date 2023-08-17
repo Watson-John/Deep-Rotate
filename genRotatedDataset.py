9ae8e274ec5c2c14c9ffa3a254801c0c4032ecc7
Watson-John <70344341+Watson-John@users.noreply.github.com>
2023-08-17 11:46:11 -0700

    Update genRotatedDataset.py

--- genRotatedDataset.py
+++ genRotatedDataset.py
@@ -10,35 +10,49 @@ def rotate_image(image, angle):
     return cv2.warpAffine(image, M, (cols, rows))
 
 # Function to generate rotated images and labels
-def generate_rotated_dataset(coco_dataset_path, output_dir):
+def generate_rotated_dataset(dataset_path, output_dir):
     rotation_angles = [0, 90, 180, 270]
-    image_list = os.listdir(coco_dataset_path)
+    image_list = []
 
-    # Initialize the progress bar
-    progress_bar = tqdm(total=len(image_list) * len(rotation_angles), desc="Processing", unit="image")
+    try:
+        for root, _, files in os.walk(dataset_path):
+            for file in files:
+                if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                    image_list.append(os.path.join(root, file))
 
-    for image_name in image_list:
-        image_path = os.path.join(coco_dataset_path, image_name)
-        image = cv2.imread(image_path)
+        progress_bar = tqdm(total=len(image_list) * len(rotation_angles), desc="Processing", unit="image")
 
-        # Rotate and save images under the corresponding angle subdirectory
-        for angle in rotation_angles:
-            rotated_image = rotate_image(image, angle)
-            angle_dir = os.path.join(output_dir, f"{angle}_Degrees")
-            os.makedirs(angle_dir, exist_ok=True)
-            rotated_image_name = f"{angle}_img{len(os.listdir(angle_dir)) + 1}.jpg"
-            rotated_image_path = os.path.join(angle_dir, rotated_image_name)
-            cv2.imwrite(rotated_image_path, rotated_image)
+        for image_path in image_list:
+            try:
+                image = cv2.imread(image_path)
+                if image is None:
+                    print(f"Error: Unable to read image '{image_path}'")
+                    progress_bar.update(len(rotation_angles))
+                    continue
 
-            # Update the progress bar
-            progress_bar.update(1)
+                for angle in rotation_angles:
+                    rotated_image = rotate_image(image, angle)
+                    angle_dir = os.path.join(output_dir, f"{angle}_Degrees")
+                    os.makedirs(angle_dir, exist_ok=True)
+                    rotated_image_name = f"{angle}_img{len(os.listdir(angle_dir)) + 1}.jpg"
+                    rotated_image_path = os.path.join(angle_dir, rotated_image_name)
+                    cv2.imwrite(rotated_image_path, rotated_image)
+                    progress_bar.update(1)
+            
+            except Exception as e:
+                print(f"Error processing image '{image_path}': {e}")
+                progress_bar.update(len(rotation_angles))
+        
+        progress_bar.close()
 
-    # Close the progress bar when the loop is finished
-    progress_bar.close()
+    except KeyboardInterrupt:
+        print("Processing interrupted.")
+    finally:
+        print("Processing completed.")
 
 # Specify the paths
-dataset_path = "Dataset/flickr30k_images"
-output_dir = "Dataset/Rotated Images"
+dataset_path = "Instagram/"
+output_dir = "Dataset/insta"
 
 # Generate the rotated dataset
 generate_rotated_dataset(dataset_path, output_dir)
